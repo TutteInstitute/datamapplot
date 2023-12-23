6054d9314f324138a2d1fbb3b23c4f1d99beaab3
Leland McInnes <leland.mcinnes@gmail.com>
2023-12-23 01:40:44 +0000

    Expose palette options to create_plot

--- datamapplot/__init__.py
+++ datamapplot/__init__.py
@@ -15,6 +15,7 @@ from datamapplot.plot_rendering import render_plot
 def create_plot(
     data_map_coords,
     labels,
+    *,
     title=None,
     sub_title=None,
     noise_label="Unlabelled",
@@ -28,6 +29,8 @@ def create_plot(
     force_matplotlib=False,
     darkmode=False,
     highlight_labels=None,
+    palette_lightiness_sampling_arc=np.pi / 8,
+    palette_hue_radius_dependence=1.0,
     **render_plot_kwds,
 ):
     cluster_label_vector = np.asarray(labels)
@@ -52,7 +55,12 @@ def create_plot(
 
     # If we don't have a color map, generate one
     if label_color_map is None:
-        palette = palette_from_datamap(data_map_coords, label_locations)
+        palette = palette_from_datamap(
+            data_map_coords,
+            label_locations,
+            theta_range=palette_lightiness_sampling_arc,
+            radius_weight_power=palette_hue_radius_dependence,
+        )
         label_to_index_map = {
             name: index for index, name in enumerate(unique_non_noise_labels)
         }
@@ -92,7 +100,9 @@ def create_plot(
         cluster_sizes = np.sqrt(pd.Series(cluster_label_vector).value_counts())
         label_size_adjustments = cluster_sizes - cluster_sizes.min()
         label_size_adjustments /= label_size_adjustments.max()
-        label_size_adjustments *= render_plot_kwds.get("label_font_size", font_scale_factor) + 2
+        label_size_adjustments *= (
+            render_plot_kwds.get("label_font_size", font_scale_factor) + 2
+        )
         label_size_adjustments = dict(label_size_adjustments - 2)
         label_size_adjustments = [
             label_size_adjustments[x] for x in unique_non_noise_labels
