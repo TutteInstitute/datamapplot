edc7f3ec9afa50239e9726be15869c5256a0e386
Leland McInnes <leland.mcinnes@gmail.com>
2024-01-06 16:09:42 -0500

    Docstring for create_plot

--- datamapplot/__init__.py
+++ datamapplot/__init__.py
@@ -35,6 +35,106 @@ def create_plot(
     use_medoids=False,
     **render_plot_kwds,
 ):
+    """Create a static plot from ``data_map_coords`` with text labels provided by ``labels``.
+    This is the primary function for DataMapPlot and provides the easiest interface to the
+    static plotting functionality. This function provides a number of options, but also
+    passes any further keyword options through to the lower level ``render_plot`` function
+    so be sure to check the documentation for ``render_plot`` to discover further keyword
+    arguments that can be used here as well.
+
+    Parameters
+    ----------
+    data_map_coords: ndarray of floats of shape (n_samples, 2)
+        The 2D coordinates for the data map. Usually this is produced via a
+        dimension reduction technique such as UMAP, t-SNE, PacMAP, PyMDE etc.
+
+    labels: ndarray of strings (object) of shape (n_samples,)
+        A string label each data point in the data map. There should ideally by
+        only up to 64 unique labels. Noise or unlabelled points should have the
+        same label as ``noise_label``, which is "Unlabelled" by default.
+
+    title: str or None (optional, default=None)
+        A title for the plot. If ``None`` then no title is used for the plot.
+        The title should be succint; three to seven words.
+
+    sub_title: str or None (optional, default=None)
+        A sub-title for the plot. If ``None`` then no sub-title is used for the plot.
+        The sub-title can be significantly longer then the title and provide more information\
+        about the plot and data sources.
+
+    noise_label: str (optional, default="Unlabelled")
+        The string used in the ``labels`` array to identify the unlabelled or noise points
+        in the dataset.
+
+    noise_color: str (optional, default="#999999")
+        The colour to use for unlabelled or noise points in the data map. This should usually
+        be a muted or neutral colour to distinguish background points from the labelled clusters.
+
+    color_label_text: bool (optional, default=True)
+        Whether to use colours for the text labels generated in the plot. If ``False`` then
+        the text labels will default to either black or white depending on ``darkmode``.
+
+    label_wrap_width: int (optional, default=16)
+        The number of characters to apply text-wrapping at when creating text labels for
+        display in the plot. Note that long words will not be broken, so you can choose
+        relatively small values if you want tight text-wrapping.
+
+    label_color_map: dict or None (optional, default=None)
+        A colour mapping to use to colour points/clusters in the data map. The mapping should
+        be keyed by the unique cluster labels in ``labels`` and take values that are hex-string
+        representations of colours. If ``None`` then a colour mapping will be auto-generated.
+
+    figsize: (int, int) (optional, default=(12,12))
+        How big to make the figure in inches (actual pixel size will depend on ``dpi``).
+
+    dynamic_label_size: bool (optional, default=False)
+        Whether to dynamically resize the text labels based on the relative sizes of the
+        clusters. This can be useful to help highlight larger clusters.
+
+    dpi: int (optional, default=plt.rcParams["figure.dpi"])
+        The dots-per-inch setting usd when rendering the plot.
+
+    force_matplotlib: bool (optional, default=False)
+        Force using matplotlib instead of datashader for rendering the scatterplot of the
+        data map. This can be useful if you wish to have a different marker_type, or variably
+        sized markers based on a marker_size_array, neither of which are supported by the
+        datashader based renderer.
+
+    darkmode: bool (optional, default=False)
+        Whether to render the plot in darkmode (with a dark background) or not.
+
+    highlight_labels: list of str or None (optional, default=None)
+        A list of unique labels that should have their text highlighted in the resulting plot.
+        Arguments supported by ``render_plot`` can allow for control over how highlighted labels
+        are rendered. By default they are simply rendered in bold text.
+
+    palette_hue_shift: float (optional, default=0.0)
+        A setting, in degrees clockwise, to shift the hue channel when generating a colour
+        palette and color_mapping for the labels.
+
+    palette_hue_radius_dependence: float (optional, default=1.0)
+        A setting that determines how dependent on the radius the hue channel is. Larger
+        values will result in more hue variation where there are more outlying points.
+
+    use_medoids: bool (optional, default=False)
+        Whether to use medoids instead of centroids to determine the "location" of the cluster,
+        both for the label indicator line, and for palette colouring. Note that medoids are
+        more computationally expensive, especially for large plots, so use with some caution.
+
+    **render_plot_kwds
+        All opther keyword arguments are passed through the ``render_plot`` which provides
+        significant further control over the aesthetics of the plot.
+
+    Returns
+    -------
+
+    fig: matplotlib.Figure
+        The figure that the resulting plot is rendered to.
+
+    ax: matpolotlib.Axes
+        The axes contained within the figure that the plot is rendered to.
+
+    """
     cluster_label_vector = np.asarray(labels)
     unique_non_noise_labels = [
         label for label in np.unique(cluster_label_vector) if label != noise_label
