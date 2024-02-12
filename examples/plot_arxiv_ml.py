46cd36b131a9ae8a3806b132826d147674e4a331
Leland McInnes <leland.mcinnes@gmail.com>
2024-02-12 15:51:02 -0500

    Google fonts now happen automatically (in theory)

--- examples/plot_arxiv_ml.py
+++ examples/plot_arxiv_ml.py
@@ -8,25 +8,9 @@ import datamapplot
 import numpy as np
 import requests
 import PIL
-import matplotlib.font_manager
 import matplotlib.pyplot as plt
-from tempfile import NamedTemporaryFile
-import re
 
 plt.rcParams['savefig.bbox'] = 'tight'
-
-def get_google_font(fontname):
-    api_fontname = fontname.replace(' ', '+')
-    api_response = resp = requests.get(
-        f"https://fonts.googleapis.com/css?family={api_fontname}:black,bold,regular,light"
-    )
-    font_urls = re.findall(r'(https?://[^\)]+)', str(api_response.content))
-    for font_url in font_urls:
-        font_data = requests.get(font_url)
-        f = NamedTemporaryFile(delete=False, suffix='.ttf')
-        f.write(font_data.content)
-        f.close()
-        matplotlib.font_manager.fontManager.addfont(f.name)
 
 arxivml_data_map = np.load("arxiv_ml_data_map.npy")
 arxivml_labels = np.load("arxiv_ml_cluster_labels.npy", allow_pickle=True)
@@ -37,7 +21,6 @@ arxiv_logo_response = requests.get(
     headers={'User-Agent': 'My User Agent 1.0'}
 )
 arxiv_logo = np.asarray(PIL.Image.open(arxiv_logo_response.raw))
-get_google_font("Playfair Display SC")
 
 fig, ax = datamapplot.create_plot(
     arxivml_data_map,
