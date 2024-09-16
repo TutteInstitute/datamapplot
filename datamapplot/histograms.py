3109a59287a0d7b80268b049cdee2352b9dd0516
Leland McInnes <leland.mcinnes@gmail.com>
2024-09-16 02:07:32 +0000

    Generate histogram bin data in python

---
+++ datamapplot/histograms.py
@@ -0,0 +1,181 @@
+import pandas as pd
+import numpy as np
+
+_NAME_TO_FREQ_CODE = {
+    "year": "YS",
+    "y": "YS",
+    "quarter": "QS",
+    "Q": "QS",
+    "q": "QS",
+    "month": "MS",
+    "M": "MS",
+    "day": "D",
+    "d": "D",
+    "hour": "H",
+    "h": "H",
+    "minute": "T",
+    "min": "T",
+    "m": "T",
+    "second": "S",
+    "sec": "S",
+    "s": "S",
+}
+
+_NAME_TO_PERIOD_CODE = {
+    "year": "Y",
+    "y": "Y",
+    "quarter": "Q",
+    "Q": "Q",
+    "q": "Q",
+    "month": "M",
+    "M": "",
+    "day": "D",
+    "d": "D",
+    "hour": "h",
+    "h": "h",
+    "minute": "min",
+    "min": "min",
+    "m": "min",
+    "second": "s",
+    "sec": "s",
+    "s": "s",
+}
+
+def generate_bins_from_numeric_data(data: pd.Series, n_bins: int = 20):
+    """
+    Generate bins for numeric data.
+    
+    Parameters
+    ----------
+    data : pd.Series
+        The data to bin.
+    n_bins : int
+        The number of bins to generate.
+        
+    Returns
+    -------
+    pd.DataFrame
+    A DataFrame with the following columns:
+        - id: The bin id.
+        - count: The number of data points in the bin.
+        - indices: The indices of the data points in the bin.
+        - min_value: The minimum value of the bin.
+        - max_value: The maximum value of the bin.
+        - mean_value: The mean value of the bin.
+    """
+    bins_ids, bin_edges = pd.cut(data, bins=n_bins, labels=False, retbins=True)
+    all_bins = pd.DataFrame({"id": range(n_bins)})
+
+    bin_data = (
+        data.groupby(bins_ids, group_keys=True)
+        .agg(count="count", indices=lambda x: list(x.index))
+        .reset_index(names=["id"])
+    )
+
+    bin_data = all_bins.merge(bin_data, on="id", how="left")
+
+    bin_data["min_value"] = bin_edges[:-1]
+    bin_data["max_value"] = bin_edges[1:]
+    # lower bin plus difference over two to allow to dates/times to work with averages as well
+    bin_data["mean_value"] = bin_edges[:-1] + (bin_edges[1:] - bin_edges[:-1]) / 2
+    bin_data["count"] = bin_data["count"].fillna(0)
+    bin_data["indices"] = bin_data["indices"].apply(
+        lambda x: x if isinstance(x, list) else []
+    )
+
+    return bin_data, bins_ids.astype(np.int16)
+
+
+def generate_bins_from_categorical_data(data: pd.Series, max_bins: int = 20):
+    """
+    Generate bins for categorical data.
+
+    Parameters
+    ----------
+    data : pd.Series
+        The data to bin.
+    max_bins : int
+        The maximum number of bins to generate.
+
+    Returns
+    -------
+    pd.DataFrame
+    A DataFrame with the following columns:
+        - id: The bin id.
+        - count: The number of data points in the bin.
+        - indices: The indices of the data points in the bin.
+        - min_value: The minimum value of the bin.
+        - max_value: The maximum value of the bin.
+        - mean_value: The mean value of the bin.
+    """
+    top_values = data.value_counts().head(max_bins - 1).index
+    bins_ids = data.apply(lambda x: x if x in top_values else "Other")
+    bin_data = (
+        data.groupby(bins_ids)
+        .agg(count="count", indices=lambda x: list(x.index))
+        .reset_index(names=["label"])
+        .reset_index(names=["id"])
+    )
+
+    bin_data["min_value"] = bin_data["id"]
+    bin_data["max_value"] = bin_data["id"]
+    bin_data["mean_value"] = bin_data["label"]
+    bin_data["count"] = bin_data["count"].fillna(0)
+    bin_data["indices"] = bin_data["indices"].apply(
+        lambda x: x if isinstance(x, list) else []
+    )
+
+    return bin_data, bins_ids.map(dict(bin_data[["label", "id"]].values)).astype(np.int16)
+
+def generate_bins_from_temporal_data(data: pd.Series, group_by: str = "year"):
+    """
+    Generate bins for temporal data.
+
+    Parameters
+    ----------
+    data : pd.Series
+        The data to bin.
+    group_by : str
+        The temporal unit to group by. Can be one of "year", "quarter", "month", "day", "hour", "minute", or "second".
+
+    Returns
+    -------
+    pd.DataFrame
+    A DataFrame with the following columns:
+        - id: The bin id.
+        - count: The number of data points in the bin.
+        - indices: The indices of the data points in the bin.
+        - min_value: The minimum value of the bin.
+        - max_value: The maximum value of the bin.
+        - mean_value: The mean value of the bin.
+    """
+    frequency_code = _NAME_TO_FREQ_CODE.get(group_by, group_by)
+    period_code = _NAME_TO_PERIOD_CODE[group_by]
+    from_date = data.min().to_period(period_code).to_timestamp()
+    to_date = (data.max().to_period(period_code) + 1).to_timestamp()
+    bins_ids, bin_edges = pd.cut(
+        data,
+        pd.date_range(from_date, to_date, freq=frequency_code),
+        labels=False,
+        retbins=True,
+    )
+    all_bins = pd.DataFrame({"id": range(len(bin_edges) - 1)})
+
+    bin_data = (
+        data.groupby(bins_ids, group_keys=True)
+        .agg(count="count", indices=lambda x: list(x.index))
+        .reset_index(names=["id"])
+    )
+
+    bin_data = all_bins.merge(bin_data, on="id", how="left")
+
+    bin_data["min_value"] = bin_edges[:-1]
+    bin_data["max_value"] = bin_edges[1:]
+    # lower bin plus difference over two to allow to dates/times to work with averages as well
+    bin_data["mean_value"] = bin_edges[:-1] + (bin_edges[1:] - bin_edges[:-1]) / 2
+    bin_data["count"] = bin_data["count"].fillna(0)
+    bin_data["indices"] = bin_data["indices"].apply(
+        lambda x: x if isinstance(x, list) else []
+    )
+
+    return bin_data, bins_ids.astype(np.int16)
