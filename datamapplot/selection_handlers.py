f65e5e8a5fc6d7fe6b35299bdefdecaebd79c17a
Leland McInnes <leland.mcinnes@gmail.com>
2024-09-21 19:26:24 +0000

    Add a word cloud selection handler

--- datamapplot/selection_handlers.py
+++ datamapplot/selection_handlers.py
@@ -1,3 +1,6 @@
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import string
+
 
 class SelectionHandlerBase:
 
@@ -25,7 +28,7 @@ class DisplaySample(SelectionHandlerBase):
     def __init__(self, n_samples=256, font_family=None, **kwargs):
         super().__init__(dependencies=["https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"], **kwargs)
         self.n_samples = n_samples
-        self.font_family = "Roboto, sans-serif" if font_family is None else font_family
+        self.font_family = font_family
 
     @property
     def javascript(self):
@@ -74,7 +77,7 @@ function lassoSelectionCallback(selectedPoints) {{
 
 function resampleSelection() {{
     const n_samples = {self.n_samples};
-    let selectedPoints = Array.from(dataSelectionManager.getSelectedIndices());
+    let selectedPoints = Array.from(datamap.getSelectedIndices());
     if (selectedPoints.length > n_samples) {{
         selectedPoints = sampleSize(selectedPoints, n_samples);
     }}
@@ -104,6 +107,10 @@ function clearSelection() {{
     
     @property
     def css(self):
+        if self.font_family:
+            font_family_str = f"font-family: {self.font_family};"
+        else:
+            font_family_str = ""
         return f"""
     #selection-container {{
         display: none;
@@ -113,7 +120,7 @@ function clearSelection() {{
         height: 95%;
         max-width: 33%;
         z-index: 10;
-        font-family: {self.font_family};
+        {font_family_str}
     }}
     #selection-display {{
         overflow-y: auto;
@@ -157,6 +164,136 @@ function clearSelection() {{
         <div id="selection-display"></div>
     </div>
         """
+    
+
+class WordCloud (SelectionHandlerBase):
+    
+    def __init__(self, n_words=256, width=500, height=500, font_family=None, stop_words=None, n_rotations=0, color_scale="YlGnBu", **kwargs):
+        super().__init__(
+            dependencies=[
+                "https://d3js.org/d3.v5.min.js", 
+                "https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.min.js",
+                "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+            ], 
+            **kwargs
+        )
+        self.n_words = n_words
+        self.width = width
+        self.height = height
+        self.font_family = font_family
+        self.stop_words = stop_words or list(ENGLISH_STOP_WORDS)
+        self.n_rotations = min(22, n_rotations)
+        if color_scale.endswith("_r"):
+            self.color_scale = string.capwords(color_scale[:1]) + color_scale[1:-2]
+            self.color_scale_reversed = True
+        else:
+            self.color_scale = string.capwords(color_scale)
+            self.color_scale_reversed = False
+ 
+    @property
+    def javascript(self):
+        return f"""
+const _STOPWORDS = new Set({self.stop_words});
+const _ROTATIONS = [0, -90, 90, -45, 45, -30, 30, -60, 60, -15, 15, -75, 75, -7.5, 7.5, -22.5, 22.5, -52.5, 52.5, -37.5, 37.5, -67.5, 67.5];
+const svg = d3.select("#word-cloud").append("svg")
+    .attr("width", {self.width})
+    .attr("height", {self.height})
+    .append("g")
+    .attr("transform", "translate(" + {self.width} / 2 + "," + {self.height} / 2 + ")");
+const wordCloudItem = document.getElementById("word-cloud");
+
+function wordCounter(textItems) {{
+    const words = textItems.join(' ').toLowerCase().split(/\s+/);
+    const wordCounts = new Map();
+    words.forEach(word => {{
+        wordCounts.set(word, (wordCounts.get(word) || 0) + 1);
+    }});
+    _STOPWORDS.forEach(stopword => wordCounts.delete(stopword));
+    const result = Array.from(wordCounts, ([word, frequency]) => ({{ text: word, size: Math.sqrt(frequency) }}))
+                        .sort((a, b) => b.size - a.size).slice(0, {self.n_words});
+    const maxSize = Math.max(...(result.map(x => x.size)));
+    return result.map(({{text, size}}) => ({{ text: text, size: (size / maxSize)}}));
+}}
+
+function generateWordCloud(words) {{
+  const width = {self.width};
+  const height = {self.height};
+
+  const colorScale = d3.scaleSequential(d3.interpolate{self.color_scale}).domain([{"width / 10, 0" if self.color_scale_reversed else "0, width / 10"}]);
+
+  // Configure a cloud layout
+  const layout = d3.layout.cloud()
+    .size([width, height])
+    .words(words.map(d => ({{text: d.text, size: d.size * width / 10}})))
+    .padding(1)
+    .rotate(() => _ROTATIONS[~~(Math.random() * {self.n_rotations})])
+    .font("{self.font_family or 'Impact'}")
+    .fontSize(d => d.size)
+    .fontWeight(d => Math.max(300, Math.min(d.size * 9000 / width, 900)))
+    .on("end", draw);
+
+  layout.start();
+
+  function draw(words) {{
+    const t = d3.transition().duration(500);
+    
+    // Update existing words
+    const text = svg.selectAll("text")
+      .data(words, d => d.text);
+    
+    // Remove old words
+    text.exit()
+      .transition(t)
+      .attr("fill-opacity", 0)
+      .attr("font-size", 1)
+      .remove();
+    // Add new words
+    text.enter()
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("fill-opacity", 0)
+      .attr("font-size", 1)
+      .attr("font-family", "{self.font_family or 'Impact'}")
+      .text(d => d.text)
+      .merge(text) // Merge enter and update selections
+      .transition(t)
+      .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+      .attr("fill-opacity", 1)
+      .attr("font-size", d => d.size)
+      .attr("font-weight", d => Math.max(300, Math.min(d.size * 9000 / width, 900)))
+      .attr("fill", d => colorScale(d.size));
+  }}
+}}
+
+function lassoSelectionCallback(selectedPoints) {{
+    if (selectedPoints.length > 0) {{
+        $(wordCloudItem).animate({{height:'show'}}, 250);
+    }} else {{
+        $(wordCloudItem).animate({{height:'hide'}}, 250);
+    }}
+    const selectedText = selectedPoints.map(i => datamap.metaData.hover_text[i]);
+    const wordCounts = wordCounter(selectedText);
+    generateWordCloud(wordCounts);
+}}
+"""
+
+    @property
+    def html(self):
+        return """<div id="word-cloud" class="container-box"></div>"""
+
+    @property
+    def css(self):
+        return f"""
+#word-cloud {{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    display: none;
+    width: {self.width}px;
+    height: {self.height}px;
+    z-index: 10;
+}}
+"""
     
 
 class TagSelection(SelectionHandlerBase):
