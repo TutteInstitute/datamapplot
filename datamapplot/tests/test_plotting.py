809c31a50d419c5cac7d3279c2130de15dbd1cda
Amy Wooding <36967030+acwooding@users.noreply.github.com>
2024-12-12 14:15:59 -0500

    initial test setup for static examples

--- datamapplot/tests/test_plotting.py
+++ datamapplot/tests/test_plotting.py
@@ -1,4 +1,71 @@
-import datamapplot
+import numpy as np
+from pathlib import Path
+import pytest
+import matplotlib.image as mpimg
+from pathlib import Path
+import importlib.util
+import requests
+import sys
+import pytest
+import matplotlib.pyplot as plt
 
-def test_plotting():
-    pass
+
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default')
+def test_plot_simple_arxiv(
+    examples_dir, 
+    mock_plt_show,
+    mock_image_requests, 
+    change_np_load_path
+):
+    mock_image_requests([
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+    ])
+    return run_static_examples_script('plot_simple_arxiv.py', examples_dir, change_np_load_path)
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default')
+def test_plot_arxiv_ml(
+    examples_dir, 
+    mock_plt_show,
+    mock_image_requests, 
+    change_np_load_path
+):
+    mock_image_requests([
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+    ])
+    return run_static_examples_script('plot_arxiv_ml.py', examples_dir, change_np_load_path)
+
+def run_static_examples_script(script_filename, script_dir, change_np_load_path):
+    """
+    Run an example script located in the examples directory with static output.
+
+    Args:
+        script_name (str): The name of the script to run (e.g., 'plot_arxiv_ml.py').
+        examples_dir (str or Path): Path to the directory containing the script.
+        change_np_load_path: Test mock for changing the loading path for np.load
+
+    Returns:
+        None
+    """
+    with change_np_load_path(script_dir):
+        script_dir = Path(script_dir)
+        script_name = Path(script_filename).stem
+
+        script_path = script_dir / script_filename
+
+        # Load and execute the script dynamically so we can use mocking
+        spec = importlib.util.spec_from_file_location(
+            script_name, str(script_path)
+        )
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[script_name] = module
+        spec.loader.exec_module(module)
+    return plt.gcf()
+
+
+def test_mock_image_requests(mock_image_requests):
+    mock_image_requests([
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png'
+    ])
+    response = requests.get('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png')
+    assert response.raw is not None
