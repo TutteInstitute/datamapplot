78caf1d2145ba59401fad22ca3b48b8262b53e65
Leland McInnes <leland.mcinnes@gmail.com>
2025-01-03 17:20:45 +0000

    Remove unused notebook non-inline worker code

--- datamapplot/interactive_rendering.py
+++ datamapplot/interactive_rendering.py
@@ -90,45 +90,6 @@ _TOOL_TIP_CSS = """
             max-width: 25%;
 """
 
-# _NOTEBOOK_NON_INLINE_WORKER = """
-#     const parsingWorkerBlob = new Blob([`
-#       async function DecompressBytes(bytes) {
-#           const blob = new Blob([bytes]);
-#           const decompressedStream = blob.stream().pipeThrough(
-#             new DecompressionStream("gzip")
-#           );
-#           const arr = await new Response(decompressedStream).arrayBuffer()
-#           return new Uint8Array(arr);
-#       }
-#       async function decodeBase64(base64) {
-#           return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
-#       }
-#       async function decompressFile(filename) {
-#           const response = await fetch(filename, {
-#             headers: {Authorization: 'Token API_TOKEN'}
-#           });
-#           if (!response.ok) {
-#             throw new Error(\`HTTP error! status: \${response.status}. Failed to fetch: \${filename}\`);
-#           }
-#           const data = await response.json()
-#             .then(data => data.content)
-#             .then(base64data => decodeBase64(base64data))
-#             .then(buffer => DecompressBytes(buffer));
-#           return data;
-#       }
-#       self.onmessage = async function(event) {
-#         const { encodedData, JSONParse } = event.data;
-#         const binaryData = await decompressFile(encodedData);
-#         if (JSONParse) {
-#           const parsedData = JSON.parse(new TextDecoder("utf-8").decode(binaryData));
-#           self.postMessage({ data: parsedData });
-#         } else {
-#           // Send the parsed table back to the main thread
-#           self.postMessage({ data: binaryData });
-#         }
-#       }
-#     `], { type: 'application/javascript' });
-# """
 _NOTEBOOK_NON_INLINE_WORKER = """
     const parsingWorkerBlob = new Blob([`
       self.onmessage = async function(event) {
