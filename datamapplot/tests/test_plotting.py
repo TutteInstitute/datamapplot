6c62593f1658a36a53c119890b5763db785abcc9
Amy Wooding <36967030+acwooding@users.noreply.github.com>
2025-01-06 16:11:52 -0800

    add the rest of the static examples

--- datamapplot/tests/test_plotting.py
+++ datamapplot/tests/test_plotting.py
@@ -9,26 +9,24 @@ import sys
 import pytest
 import matplotlib.pyplot as plt
 
-
-
 @pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default')
-def test_plot_simple_arxiv(
-    examples_dir, 
+def test_plot_cord19(
+    examples_dir,
     mock_plt_show,
-    mock_image_requests, 
+    mock_image_requests,
     mock_savefig,
     change_np_load_path
 ):
     mock_image_requests([
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+        "https://allenai.org/newsletters/archive/2023-03-newsletter_files/927c3ca8-6c75-862c-ee5d-81703ef10a8d.png"
     ])
-    return run_static_examples_script('plot_simple_arxiv.py', examples_dir, change_np_load_path, mock_savefig)
+    return run_static_examples_script('plot_cord19.py', examples_dir, change_np_load_path, mock_savefig)
 
 @pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default')
 def test_plot_arxiv_ml(
-    examples_dir, 
+    examples_dir,
     mock_plt_show,
-    mock_image_requests, 
+    mock_image_requests,
     mock_savefig,
     change_np_load_path
 ):
@@ -36,6 +34,45 @@ def test_plot_arxiv_ml(
         "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
     ])
     return run_static_examples_script('plot_arxiv_ml.py', examples_dir, change_np_load_path, mock_savefig)
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default', tolerance=15)
+def test_plot_arxiv_ml_word_cloud(
+    examples_dir,
+    mock_plt_show,
+    mock_image_requests,
+    mock_savefig,
+    change_np_load_path
+):
+    mock_image_requests([
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+    ])
+    return run_static_examples_script('plot_arxiv_ml_word_cloud.py', examples_dir, change_np_load_path, mock_savefig)
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default', tolerance=15)
+def test_plot_wikipedia(
+    examples_dir,
+    mock_plt_show,
+    mock_image_requests,
+    mock_savefig,
+    change_np_load_path
+):
+    mock_image_requests([
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+    ])
+    return run_static_examples_script('plot_wikipedia.py', examples_dir, change_np_load_path, mock_savefig)
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline', style='default')
+def test_plot_simple_arxiv(
+    examples_dir,
+    mock_plt_show,
+    mock_image_requests,
+    mock_savefig,
+    change_np_load_path
+):
+    mock_image_requests([
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/320px-ArXiv_logo_2022.svg.png"
+    ])
+    return run_static_examples_script('plot_simple_arxiv.py', examples_dir, change_np_load_path, mock_savefig)
 
 def run_static_examples_script(script_filename, script_dir, change_np_load_path, mock_savefig):
     """
@@ -63,7 +100,6 @@ def run_static_examples_script(script_filename, script_dir, change_np_load_path,
     with change_np_load_path(script_dir), mock_savefig():
         spec.loader.exec_module(module)
     return plt.gcf()
-
 
 def test_mock_image_requests(mock_image_requests):
     mock_image_requests([
