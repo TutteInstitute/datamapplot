829567f049c3eaed0876cd1a6437183dcbd5091b
Leland McInnes <leland.mcinnes@gmail.com>
2026-01-15 17:46:41 +0000

    Fix topic tree after refactor

--- examples/plot_interactive_arxiv_ml_topic_tree.py
+++ examples/plot_interactive_arxiv_ml_topic_tree.py
@@ -13,7 +13,9 @@ arxivml_data_map = np.load("arxiv_ml_data_map.npz")["arr_0"]
 arxivml_label_layers = []
 for layer_num in range(5):
     arxivml_label_layers.append(
-        np.load(f"arxiv_ml_layer{layer_num}_cluster_labels.npz", allow_pickle=True)["arr_0"]
+        np.load(f"arxiv_ml_layer{layer_num}_cluster_labels.npz", allow_pickle=True)[
+            "arr_0"
+        ]
     )
 arxivml_hover_data = np.load("arxiv_ml_hover_data.npz", allow_pickle=True)["arr_0"]
 
@@ -22,22 +24,22 @@ plot = datamapplot.create_interactive_plot(
     arxivml_label_layers[0],
     arxivml_label_layers[2],
     arxivml_label_layers[4],
-    hover_text = arxivml_hover_data,
+    hover_text=arxivml_hover_data,
     initial_zoom_fraction=0.999,
     font_family="Playfair Display SC",
     title="ArXiv Machine Learning Landscape - With Topic Tree",
     sub_title="A data map of papers from the Machine Learning section of ArXiv",
     logo="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/ArXiv_logo_2022.svg/512px-ArXiv_logo_2022.svg.png",
     logo_width=128,
-    on_click="window.open(`http://google.com/search?q=\"{hover_text}\"`)",
+    on_click='window.open(`http://google.com/search?q="{hover_text}"`)',
     enable_search=True,
     darkmode=True,
-    inline_data=False,
+    inline_data=True,
     offline_data_prefix="arxivml_gallery_topic_tree",
     enable_topic_tree=True,
     topic_tree_kwds={
         "color_bullets": True,
-    }
+    },
 )
 plot.save("arxiv_ml_topic_tree.html")
 plot
