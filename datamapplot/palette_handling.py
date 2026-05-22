78d2a2a4548435f87d28a74f5e35c25be7c76310
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2026-05-22 22:11:53 +0000

    Ref: clean up the color association loop

--- datamapplot/palette_handling.py
+++ datamapplot/palette_handling.py
@@ -1,10 +1,15 @@
+from collections.abc import Callable, Iterable, Iterator
 from dataclasses import dataclass
 import numpy as np
 import operator
-from typing import Optional
+from typing import Optional, Protocol
 
 import colorspacious
 from matplotlib.colors import rgb2hex, to_rgb, ListedColormap
+from matplotlib.cm import hsv
+
+
+EPS = 1e-6
 
 
 @dataclass
@@ -23,13 +28,16 @@ class Polars:
             center_ = np.asarray(center)
 
         xy_centered = xy - center_
-        return cls(
+        pol = cls(
             radii=np.linalg.norm(xy_centered, axis=1),
             thetas=np.arctan2(xy_centered.T[1], xy_centered.T[0]),
             center=center_,
         )
+        assert np.all(pol.radii >= 0)
+        assert np.all((pol.thetas >= -np.pi) & (pol.thetas <= np.pi))
+        return pol
 
-    
+
 def palette_from_datamap(
     umap_coords,
     label_locations,
@@ -38,246 +46,156 @@ def palette_from_datamap(
     radius_weight_power=1.0,
     min_lightness=10,
 ):
-    if label_locations.shape[0] == 0:
-        return []
-
-    pol_data = Polars.from_xy(umap_coords)
-    pol_labels = Polars.from_xy(label_locations, center=pol_data.center)
-
-    sorter = np.argsort(pol_labels.thetas)
-    weights = (pol_labels.radii**radius_weight_power)[sorter]
-    hue = weights.cumsum()
-    hue = (hue / hue.max()) * 360
-
-    location_hue = np.interp(
-        pol_labels.thetas, np.sort(pol_labels.thetas), np.sort(hue)
+    return palette_from_cmap_and_datamap(
+        cmap=None,
+        umap_coords=umap_coords,
+        label_locations=label_locations,
+        hue_shift=hue_shift,
+        chroma_bounds=(20, 100),
+        lightness_bounds=(min_lightness, 80 - min_lightness),
+        theta_range=theta_range,
+        radius_weight_power=radius_weight_power,
     )
-    location_hue = (location_hue + hue_shift) % 360
-
-    location_chroma = []
-    location_lightness = []
-    if pol_labels.thetas.shape[0] < 256:
-        for r, theta in zip(pol_labels.radii, pol_labels.thetas):
-            # use increasing values of theta range to ensure that we find a mask containing some elements
-            for i_theta_range in np.linspace(theta_range, np.pi, 16):
-                theta_high = theta + i_theta_range
-                theta_low = theta - i_theta_range
-                if theta_high > np.pi:
-                    theta_high -= 2 * np.pi
-                if theta_low < -np.pi:
-                    theta_low += 2 * np.pi
-
-                between = operator.or_ if theta_low > 0 and theta_high < 0 else operator.and_
-                r_mask = between(pol_data.thetas > theta_low, pol_data.thetas < theta_high)
-                # if theta_low > 0 and theta_high < 0:
-                #     r_mask = (pol_data.thetas < theta_low) & (pol_data.thetas > theta_high)
-                # else:
-                #     r_mask = (pol_data.thetas > theta_low) & (pol_data.thetas < theta_high)
-
-                mask_size = np.sum(r_mask)
-                if mask_size > 0:
-                    break
-            else:
-                raise ValueError("No mask found for theta range.")
-
-            chroma = (
-                np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size
-            ) * 80 + 20
-            lightness = (
-                1.0 - (np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size)
-            ) * (80 - min_lightness) + min_lightness
-            location_lightness.append(
-                np.interp(
-                    r,
-                    np.sort(pol_data.radii[r_mask]),
-                    np.sort(lightness)[::-1],
-                )
-            )
-            location_chroma.append(
-                np.interp(r, np.sort(pol_data.radii[r_mask]), np.sort(chroma))
-            )
-    else:
-        uniform_thetas = np.linspace(-np.pi, np.pi, 256)
-        sorted_chroma = []
-        sorted_lightness = []
-        sorted_radii = []
-        for theta in uniform_thetas:
-            mask_size = 0
-            for theta_spread in np.linspace(theta_range, np.pi, num=16):
-                theta_high = theta + theta_spread
-                theta_low = theta - theta_spread
-                if theta_high > np.pi:
-                    theta_high -= 2 * np.pi
-                if theta_low < -np.pi:
-                    theta_low += 2 * np.pi
-
-                between = operator.or_ if theta_low > 0 and theta_high < 0 else operator.and_
-                r_mask = between(pol_data.thetas > theta_low, pol_data.thetas < theta_high)
-                # if theta_low > 0 and theta_high < 0:
-                #     r_mask = (pol_data.thetas < theta_low) & (pol_data.thetas > theta_high)
-                # else:
-                #     r_mask = (pol_data.thetas > theta_low) & (pol_data.thetas < theta_high)
-                mask_size = np.sum(r_mask)
-                if mask_size > 0:
-                    break
-            else:
-                raise RuntimeError(f"Cannot reference data for computing chroma and lightness for label at angle {theta}")
-            assert mask_size > 0
-
-            # mask_size = np.sum(r_mask)
-            chroma = (
-                np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size
-            ) * 80 + 20
-            lightness = (
-                1.0 - (np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size)
-            ) * (80 - min_lightness) + min_lightness
-            sorted_chroma.append(np.sort(chroma))
-            sorted_lightness.append(np.sort(lightness)[::-1])
-            sorted_radii.append(np.sort(pol_data.radii[r_mask]))
-
-        for r, theta in zip(pol_labels.radii, pol_labels.thetas):
-            nearest_theta_idx = np.argmin(np.abs(uniform_thetas - theta))
-            location_lightness.append(
-                np.interp(
-                    r,
-                    sorted_radii[nearest_theta_idx],
-                    sorted_lightness[nearest_theta_idx],
-                )
-            )
-            location_chroma.append(
-                np.interp(
-                    r, sorted_radii[nearest_theta_idx], sorted_chroma[nearest_theta_idx]
-                )
-            )
-
-    palette = np.clip(
-        colorspacious.cspace_convert(
-            np.vstack(
-                (
-                    np.asarray(location_lightness),
-                    np.asarray(location_chroma),
-                    location_hue,
-                )
-            ).T,
-            "JCh",
-            "sRGB1",
-        ),
-        0,
-        1,
-    )
-    return [rgb2hex(color) for color in palette]
 
 
-def scaling_func(xs, lo, mid, hi):
+def scaling_parabolic(xs, lo, mid, hi):
     vals = (2 * (lo + hi) - 4 * mid) * xs**2 + (4 * mid - 3 * lo - hi) * xs + lo
     return np.clip(vals, lo, hi)
+
+
+Scaler = Callable[[float], float]
+Bounds = tuple[float, float]
+
+class ColorSet(Protocol):
+
+    def hues(self) -> np.ndarray: ...
+    def scalers(self, bounds_chroma: Bounds, bounds_lightness: Bounds) -> Iterator[tuple[Scaler, Scaler]]: ...
+
+
+@dataclass
+class ColorsUntethered:
+    weights: np.ndarray
+
+    def hues(self) -> np.ndarray:
+        return self.weights * 360.
+
+    def scalers(self, bounds_chroma: Bounds, bounds_lightness: Bounds) -> Iterator[tuple[Scaler, Scaler]]:
+        def linear01(bounds: Bounds) -> Scaler:
+            return lambda x: bounds[0] + x * (bounds[1] - bounds[0])
+
+        scale_chroma = linear01(bounds_chroma)
+        scale_lightness = linear01(bounds_lightness)
+        for _ in self.weights:
+            yield scale_chroma, scale_lightness
+
+
+class ColorsThroughMap:
+
+    def __init__(self, weights: float, cmap: Callable[[Iterable[float]], np.ndarray]) -> None:
+        endpoints = cmap((0.0, 1.0))
+        endpoint_distance = np.sum((endpoints[0] - endpoints[1]) ** 2)
+        if endpoint_distance < 0.05:
+            cyclic_cmap = cmap
+        else:
+            new_colors = np.vstack(
+                (
+                    cmap(np.linspace(0, 1, 128)),
+                    cmap(np.linspace(1, 0, 128)),
+                )
+            )
+            cyclic_cmap = ListedColormap(new_colors, name="generated_cyclic_cmap")
+
+        rgb = cyclic_cmap(weights)[:, :3]
+        self.jch = colorspacious.cspace_convert(rgb, "sRGB1", "JCh")
+
+    def hues(self) -> np.ndarray:
+        return self.jch.T[2]
+
+    def scalers(self, bounds_chroma: Bounds, bounds_lightness: Bounds) -> Iterator[tuple[Scaler, Scaler]]:
+        for lightness, chroma, _ in self.jch:
+            yield (
+                lambda x: scaling_parabolic(x, bounds_chroma[0], chroma, bounds_chroma[1]),
+                lambda x: scaling_parabolic(x, bounds_lightness[0], lightness, bounds_lightness[1])
+            )
 
 
 def palette_from_cmap_and_datamap(
     cmap,
     umap_coords,
     label_locations,
+    hue_shift=0.,
     chroma_bounds=(20, 90),
     lightness_bounds=(10, 80),
     theta_range=np.pi / 16,
     radius_weight_power=1.0,
+    sector_scan=None,
 ):
     if label_locations.shape[0] == 0:
-        return [cmap(0.5)]
-
-    endpoints = cmap((0.0, 1.0))
-    endpoint_distance = np.sum((endpoints[0] - endpoints[1]) ** 2)
-    if endpoint_distance < 0.05:
-        cyclic_cmap = cmap
-    else:
-        new_colors = np.vstack(
-            (
-                cmap(np.linspace(0, 1, 128)),
-                cmap(np.linspace(1, 0, 128)),
-            )
-        )
-        cyclic_cmap = ListedColormap(new_colors, name="generated_cyclic_cmap")
+        return [cmap(0.5) if cmap else f"#888888"]
 
     pol_data = Polars.from_xy(umap_coords)
     pol_labels = Polars.from_xy(label_locations, center=pol_data.center)
 
-    sorter = np.argsort(pol_labels.thetas)
-    weights = (pol_labels.radii**radius_weight_power)[sorter]
-    weights = weights.cumsum()
-    weights /= weights.max()
-
-    location_base_vals = np.interp(
-        pol_labels.thetas, np.sort(pol_labels.thetas), np.sort(weights)
+    order_labels = np.argsort(pol_labels.thetas)
+    weights_base = (pol_labels.radii**radius_weight_power)[order_labels].cumsum()
+    weights_base /= weights_base.max()
+    weights_spaced = np.interp(
+        pol_labels.thetas, pol_labels.thetas[order_labels], weights_base
     )
-    base_colors = cyclic_cmap(location_base_vals)[:, :3]
+    colorset = ColorsThroughMap(weights_spaced, cmap) if cmap else ColorsUntethered(weights_spaced)
+    hue_labels = (colorset.hues() + hue_shift) % 360
 
-    base_colors_jch = colorspacious.cspace_convert(base_colors, "sRGB1", "JCh")
+    thetas_sector_uniform = lambda: np.linspace(-np.pi, np.pi, 256, endpoint=False)
+    match sector_scan:
+        case "labels":
+            thetas_sector = pol_labels.thetas
+        case "uniform":
+            thetas_sector = thetas_sector_uniform()
+        case None:
+            thetas_sector = pol_labels.thetas if pol_labels.thetas.shape[0] < 256 else thetas_sector_uniform()
+        case _:
+            raise ValueError("Argument sector_scan must be either None, 'labels' or 'uniform'")
 
-    location_hue = base_colors_jch.T[2]
-    location_chroma = []
-    location_lightness = []
-    for i, (r, theta) in enumerate(zip(pol_labels.radii, pol_labels.thetas)):
+    radii_sector = []
+    theta_range = min(theta_range, (1. - EPS) * np.pi)
+    for theta in thetas_sector:
         mask_size = 0
-        for theta_spread in np.linspace(theta_range, np.pi, num=16):
+        for theta_spread in np.linspace(2 * theta_range, np.pi, num=16):
             theta_high = theta + .5 * theta_spread
             theta_low = theta - .5 * theta_spread
             if theta_high > np.pi:
                 theta_high -= 2 * np.pi
             if theta_low < -np.pi:
                 theta_low += 2 * np.pi
-
             between = operator.or_ if theta_low > 0 and theta_high < 0 else operator.and_
             r_mask = between(pol_data.thetas > theta_low, pol_data.thetas < theta_high)
-            #     r_mask_neg = (pol_data.thetas < 0) & 
-            #     r_mask = (pol_data.thetas < theta_low) & (pol_data.thetas > theta_high)
-            # else:
-            #     r_mask = (pol_data.thetas > theta_low) & (pol_data.thetas < theta_high)
             mask_size = np.sum(r_mask)
             if mask_size > 0:
                 break
         else:
             raise RuntimeError(f"Cannot reference data for computing chroma and lightness for label at angle {theta}")
         assert mask_size > 0
+        radii_sector.append(np.sort(pol_data.radii[r_mask]))
+    assert len(radii_sector) == len(thetas_sector)
 
-        chroma_scale = np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size
-        chroma = scaling_func(
-            chroma_scale, chroma_bounds[0], base_colors_jch[i, 1], chroma_bounds[1]
-        )
+    lightness_labels = []
+    chroma_labels = []
+    for (scale_chroma, scale_lightness), theta, radius in zip(colorset.scalers(chroma_bounds, lightness_bounds), pol_labels.thetas, pol_labels.radii):
+        i_nearest_sector = np.argmin(np.abs(thetas_sector - theta))
+        radii_reference = radii_sector[i_nearest_sector]
+        scale = np.arange(len(radii_reference)) / len(radii_reference)
 
-        lightness_scale = 1.0 - (
-            np.argsort(np.argsort(pol_data.radii[r_mask])) / mask_size
-        )
-        lightness = scaling_func(
-            lightness_scale,
-            lightness_bounds[0],
-            base_colors_jch[i, 0],
-            lightness_bounds[1],
-        )
-        location_lightness.append(
-            np.interp(
-                r,
-                np.sort(pol_data.radii[r_mask]),
-                np.sort(lightness)[::-1],
-            )
-        )
-        location_chroma.append(
-            np.interp(r, np.sort(pol_data.radii[r_mask]), np.sort(chroma))
-        )
+        chroma_labels.append(np.interp(radius, radii_reference, np.sort(scale_chroma(scale))))
+        lightness_labels.append(np.interp(radius, radii_reference, np.sort(scale_lightness(1. - scale))[::-1]))
 
     palette = np.clip(
         colorspacious.cspace_convert(
-            np.vstack(
-                (
-                    np.asarray(location_lightness),
-                    np.asarray(location_chroma),
-                    location_hue,
-                )
-            ).T,
+            np.vstack([np.asarray(p) for p in [lightness_labels, chroma_labels, hue_labels]]).T,
             "JCh",
             "sRGB1",
         ),
-        0,
-        1,
+        0.,
+        1.,
     )
     return [rgb2hex(color) for color in palette]
 
